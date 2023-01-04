762e3776904198d0de3d759639f019fed7394c02
ZJY0059 <1491627991@qq.com>
2023-01-04 17:41:38 +0800

    修改

---
+++ 1.3/src/views/dashboard.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>666666666666</div>
+</template>
+
+<script>
+import { ref, reactive, toRefs, onBeforeUnmount, onMounted } from 'vue'
+import { useStore } from 'vuex'
+import { useRouter, useRoute } from 'vue-router'
+export default {
+  setup(props, ctx) {
+    const store = useStore()
+    const router = useRouter()
+    const route = useRoute()
+    const data = reactive({})
+    return { ...toRefs(data) }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
---
+++ 1.3/src/views/home.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="common-layout">
+    <el-container>
+      <el-aside width="200px">
+        <div class="aside">
+          <img src="../assets/ihrm图标.png" alt="" />
+          <img src="../assets/IHRM下放图标.png" alt="" class="aside-imgs" />
+        </div>
+      </el-aside>
+      <el-container>
+        <el-header>Header</el-header>
+        <el-main>
+          <router-view></router-view>
+        </el-main>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+<script>
+import { ref, reactive, toRefs, onBeforeUnmount, onMounted } from 'vue'
+import { useStore } from 'vuex'
+import { useRouter, useRoute } from 'vue-router'
+export default {
+  setup(props, ctx) {
+    const store = useStore()
+    const router = useRouter()
+    const route = useRoute()
+    const data = reactive({})
+    return { ...toRefs(data) }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.el-header {
+  width: 100%;
+  height: 50px;
+  background: #4879fb;
+}
+.el-aside {
+  width: 180px;
+  background: #5485fd;
+  height: 100vh;
+  .aside {
+    width: 180px;
+    height: 80px;
+    img {
+      width: 100%;
+      height: 100%;
+    }
+    .aside-imgs {
+      margin-top: 545px;
+    }
+  }
+}
+</style>
