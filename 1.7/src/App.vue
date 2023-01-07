5bba2057925b5af8b00ebdc083f4e214289870c8
ZJY0059 <1491627991@qq.com>
2023-01-07 23:46:27 +0800

    修改

---
+++ 1.7/src/App.vue
@@ -0,0 +1,30 @@
+
+<template>
+  <el-config-provider :locale="locale">
+    <router-view></router-view>
+  </el-config-provider>
+</template>
+
+<script>
+import { ElConfigProvider } from 'element-plus'
+import zhCn from 'element-plus/lib/locale/lang/zh-cn'
+
+export default {
+  name: 'App',
+  components: {
+    ElConfigProvider
+  },
+  setup() {
+    return {
+      locale: zhCn
+    }
+  }
+}
+</script>
+<style lang="scss">
+* {
+  padding: 0;
+  margin: 0;
+  list-style: none;
+}
+</style>
---
+++ 1.7/src/views/settings/settings.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>aaaaa</div>
+</template>
+
+<script lang="ts" setup>
+import { ref, reactive, toRefs } from 'vue'
+import { FormInstance, FormRules } from 'element-plus'
+import { useRouter, useRoute } from 'vue-router'
+</script>
+
+<style lang="scss" scoped>
+</style>
+
